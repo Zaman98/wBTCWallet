5cd36407bb45d1cf915db6de40ee6c39d22cac65
Abdullah Zaman <40742761+Zaman98@users.noreply.github.com>
2022-10-04 19:45:59 +0530

    Update setup.js

--- setup.js
+++ setup.js
@@ -3,7 +3,7 @@ const Web3 = require('web3')
 // 0. Connect to the Ethereum client node
 const web3 = new Web3('https://goerli.infura.io/v3/29568a5cdf1c476ead117b51bc7c7b95')
 
-
+const fs = require('fs');
 // 1. Ask user to set a password, this would be sotes in a 
 const password = 'apples'
 var keyStoreObject = {};
@@ -17,4 +17,16 @@ const accountCreation = () => {
 }
 accountCreation()
 
-module.exports = { web3, password, keyStoreObject }
+module.exports = { web3 }
+
+const baseObject = {
+    password, keyStoreObject
+}
+
+
+fs.writeFile('new.json', JSON.stringify(baseObject), function (err) {
+    if (err) return console.log(err);
+    console.log('check new.json, baseObject copied succesfully.');
+});
+
+
