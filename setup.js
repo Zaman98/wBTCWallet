f0dfede53b078b14974cf58bcce207e713f69575
Abdullah Zaman <40742761+Zaman98@users.noreply.github.com>
2022-10-04 18:54:15 +0530

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,20 @@
+const Web3 = require('web3')
+
+// 0. Connect to the Ethereum client node
+const web3 = new Web3('https://goerli.infura.io/v3/29568a5cdf1c476ead117b51bc7c7b95')
+
+
+// 1. Ask user to set a password, this would be sotes in a 
+const password = 'apples'
+var keyStoreObject = {};
+
+// 2. Create a new EOA on Ethereum and encrypt it with the password provided by the user
+const accountCreation = () => {
+    const account = web3.eth.accounts.create()
+    const PK = account.privateKey
+    keyStoreObject = web3.eth.accounts.encrypt(PK, password)
+    //return keyStoreObject
+}
+accountCreation()
+
+module.exports = { web3, password, keyStoreObject }
